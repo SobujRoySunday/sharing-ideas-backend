c86ebffab2bc6b02e236d841a72757e4fb3f9653
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-01-10 12:45:21 +0530

    Port and url fixed

--- server.js
+++ server.js
@@ -6,19 +6,18 @@ const uploadMiddleware = multer({ dest: "uploads/" });
 const fs = require("fs");
 require("dotenv").config();
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({ credentials: true, origin: "https://sharing-ideas.glitch.me:3000" })
+);
 app.use(express.json());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.post("/uploadfile", uploadMiddleware.single("file"), async (req, res) => {
   const { originalname, path } = req.file;
-  // const parts = originalname.split(".");
-  // const ext = parts[parts.length - 1];
-  // const newPath = path + "." + ext;
   fs.renameSync(path, `uploads/${originalname}`);
   console.log(`New video added: ${originalname}`);
   res.json({ success: "ok" });
 });
 
-app.listen(process.env.PORT);
-console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(3000);
+console.log(`Server is running on port 3000`);
